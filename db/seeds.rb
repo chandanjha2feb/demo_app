55f0321a9f8cee59799033ddf0b37e3d7e73d530
chandanjha2feb <chandanjha2feb@gmail.com>
2019-10-23 22:59:11 +0530

    added role filed in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = User.create(first_name: 'admin', last_name: 'admin', email: 'admin@admin.com', password: '123456', password_confirmation: '123456')
+admin = User.create(first_name: 'admin', last_name: 'admin', email: 'admin@admin.com', password: '123456', password_confirmation: '123456', role:'admin')
 secret_code = SecretCode.new(secret_code: 'foobar')
 secret_code.user = admin
 secret_code.save
